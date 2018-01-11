aa4dd7f66dbef3c5168dc9376ea26292d52fdd39
Malcolm Jones <malcolm@adobe.com>
2018-01-11 02:01:29 -0500

    chg: more tests for swapfile

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,18 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_etc_fstab_file(host):
+    fstab = host.file("/etc/fstab")
+    assert fstab.contains("/swapfile   none    swap    sw    0   0")
+    assert fstab.user == "root"
+    assert fstab.group == "root"
+    assert fstab.mode == 0o644
+
+
+def test_swapfile(host):
+    swapfile = host.file("/swapfile")
+    assert swapfile.user == "root"
+    assert swapfile.group == "root"
+    assert swapfile.mode == 0o600
+    assert swapfile.size == 512000000
